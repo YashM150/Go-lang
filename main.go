a1d81caa1b981c50b95c9692aeb528ff5be0e46c
YashM150 <yash.moharil@bluealtair.com>
2025-02-17 11:08:56 +0530

    var declaration

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	myutil "learngo/myutils"
+)
 
-func main(){
+func main() {
 	fmt.Println("Learn go")
+	myutil.PrintMessage("Myutil: Hello World")
+
+	var num int = 1
+	var name string = "Yash"
+	var numb float64 = 20.21
+	var status bool = false
+	rando := "yash"
+	fmt.Println("Variables", num, name, numb, status, rando)
 }
